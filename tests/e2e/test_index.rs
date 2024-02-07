13af26e98e4f2e4377408498759069338a7884ef
Sparrow0hawk <alexjcoleman@gmail.com>
2024-02-07 20:53:37 +0000

    Introduce leaflet map on index

--- tests/e2e/test_index.rs
+++ tests/e2e/test_index.rs
@@ -1,9 +1,20 @@
 use crate::fixtures;
 use thirtyfour::prelude::*;
 
-async fn find_tag_text(driver: &WebDriver, tag_name: &str) -> WebDriverResult<String> {
-    let tag = driver.find(By::Tag(tag_name)).await?;
-    tag.text().await
+async fn find_element(driver: &WebDriver, by: By) -> WebDriverResult<WebElement> {
+    driver.find(by).await
+}
+
+async fn get_text(driver: &WebDriver, by: By) -> WebDriverResult<String> {
+    let element = find_element(&driver, by).await?;
+
+    element.text().await
+}
+
+async fn get_clickable(driver: &WebDriver, by: By) -> WebDriverResult<bool> {
+    let element = find_element(&driver, by).await?;
+
+    element.is_clickable().await
 }
 
 #[tokio::test]
@@ -12,18 +23,38 @@ async fn test_index_shows_hello_world() -> WebDriverResult<()> {
 
     let mut caps = DesiredCapabilities::firefox();
     caps.set_headless()?;
-    let driver = WebDriver::new("http://localhost:4444", caps)
-        .await?;
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
 
-    driver
-        .goto(&app.address)
-        .await?;
+    driver.goto(&app.address).await?;
 
-    let header_text = find_tag_text(&driver, "h1").await;
+    let by = By::Tag("h1");
+
+    let header_text = get_text(&driver, by).await;
 
     driver.quit().await?;
 
     assert_eq!(header_text?, "Map my round");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_index_shows_map() -> WebDriverResult<()> {
+    let app = fixtures::spawn_app().await;
+
+    let mut caps = DesiredCapabilities::firefox();
+    caps.set_headless()?;
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+
+    driver.goto(&app.address).await?;
+
+    let by = By::ClassName("leaflet-map-pane");
+
+    let map = get_clickable(&driver, by).await;
+
+    driver.quit().await?;
+
+    assert!(map?);
 
     Ok(())
 }
